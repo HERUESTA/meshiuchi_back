1d1036537547a554867898a0f1878c61be045fdc
seki <102042736+HERUESTA@users.noreply.github.com>
2025-05-19 21:11:32 +0900

    cors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "localhost:8000",
+            "127.0.0.1:8000",
+            "https://meshiutchi-front.vercel.app"
 
     resource "*",
       headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      methods: %i[get post put patch delete options head]
   end
 end
